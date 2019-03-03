3e276be62087b9f62e7a3b6cbb2c4f31aca82a82
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-03-03 15:29:14 +0800

    shoud be final 7f

--- 7f_canvas_sketchpad/src/App.vue
+++ 7f_canvas_sketchpad/src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
+  <div id="app ">
  
-    <ul class="navbar" :class="{hideNavBar}">
+    <ul class="navbar " 
+        :class="{hideNavBar}"
+        >
+
       <li @click="canvasToImage">
         <i class="fa fa-save"></i>
         SAVE
@@ -18,7 +21,7 @@
          <i class="fas fa-redo"></i>
         REDO
       </li>
-      <div class="btn btn-white up"></div>
+      <div class="btn up " @click="hideNavBar = !hideNavBar"></div>
     </ul>
     
     <canvas ref="sketchpad" 
@@ -28,13 +31,13 @@
       ></canvas>
 
     <ul class="toolbar" :class="{hideToolBar}">
-      
-     <li class="toolbar__tool">
+      <div class="btn btn-toolbar down" @click="hideToolBar = !hideToolBar"></div>
+      <li class="toolbar__tool">
        <i v-for="(tool,index) in tools "
           :key="index"
-          class="fas "
-          :class="`fa-${tool} `+ isToolActive(tool)" 
-          @click="currentTool=tool"
+          
+          :class="`${tool.preClass } fa-${tool.name} `+ isToolActive(tool.name)" 
+          @click="currentTool=tool.name"
           ></i>
      </li>
      <li class="toolbar__size">
@@ -54,7 +57,7 @@
         @click="currentColor=color"></div>
        
      </li>
-     <div class="btn btn-white down"></div>
+  
     </ul>
 
   </div>
@@ -77,58 +80,98 @@ export default {
         {name:'darkgreen',code:'#01936F'}],
       currentColor:null, 
       currentSize:'20',
-      isDrawing:false,
       isSizing:false, 
+      isCanvasMouseDown:false,
       hideToolBar:false,
       hideNavBar:false,
-      tools:['paint-brush','eraser'],
+      tools:[
+        {
+          preClass:'fas',
+          name:'paint-brush'
+        },{
+          preClass:'fas',
+          name:'eraser'
+        },{
+          preClass:'far',
+          name:'square'
+        }],
       currentTool:'paint-brush',
       tempPosition:null,
-
+      tempCanvas:null
     } 
   },
   methods:{
-    onCanvasMouseDown(){ this.isDrawing = true },
+    onCanvasMouseDown(){ 
+      this.isCanvasMouseDown = true
+      this.setTempCanvas()
+       
+    },
     onCanvasMouseUp(){
-      this.saveCavas()
+      this.saveCanvasToHistory()
       this.resetToolState()
     },
     onSizeMouseDown(){ this.isSizing = true },
     canvasToImage(){
-      let url =  this.$refs['sketchpad'].toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");;
+      let url =  this.$refs['sketchpad'].toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
       const link = document.createElement('a')
       link.innerText = 'Download'
-      link.href = url // 將url 設定給 a tage 的 href
-      link.download = 'circle.png'    // 設定 download name 
+      link.href = url 
+      link.download = 'circle.png'     
       link.click()
     },
     setWindowEvent(){
       window.addEventListener('mousemove',(event)=>{
            
           let currentPos = this.getCanvasMousePosition(event.clientX,event.clientY)
+          
           if(this.isSizing && this.tempPosition ){
             let dragValue= currentPos.y- this.tempPosition.y 
             this.checkSizeDrag(dragValue)
           }
 
-          if(this.isDrawing && this.tempPosition) {
+          if(this.isCanvasMouseDown && this.tempPosition){
             let pos = this.getCanvasMousePosition(event.clientX,event.clientY)
-            this.draw((ctx)=>{
-              ctx.strokeStyle = this.currentTool==='eraser'
-              ? this.backgroundColor
-              : this.currentColor.code
+            switch(this.currentTool){
+              case 'paint-brush' : 
+                    this.draw((ctx)=>{
+                        ctx.strokeStyle =  this.currentColor.code
+                        ctx.moveTo(this.tempPosition.x,this.tempPosition.y)
+                        ctx.lineTo(pos.x,pos.y)
+                    })
+                    break;
+              case 'eraser' :
+                     this.draw((ctx)=>{
+                      ctx.strokeStyle = this.backgroundColor
+                      ctx.moveTo(this.tempPosition.x,this.tempPosition.y)
+                      ctx.lineTo(pos.x,pos.y)
+                      }) 
+                    break;  
+              case 'square' :
+                    this.draw((ctx)=>{
+                      // ctx.moveTo(this.tempPosition.x,this.tempPosition.y)
+                      // ctx.lineTo(pos.x,pos.y)
+                      let currentPos = this.getCanvasMousePosition(event.clientX,event.clientY)
+                      
+                      ctx.strokeStyle =  this.currentColor.code
+                      let tempx = this.tempPosition.x
+                      let tempy = this.tempPosition.y
+                      let width = currentPos.x - tempx
+                      let height = currentPos.y - tempy
+                       
+                      ctx.putImageData(this.tempCanvas,0, 0)                      
+                      ctx.rect(tempx, tempy, width, height);
+                      
+                    })
+                    
+                    break;  
+            }
+        }
+        if(this.currentTool=='square' && this.isCanvasMouseDown )return
+        this.setCanvasTempPosition(currentPos.x,currentPos.y)
           
-              ctx.lineWidth = this.currentSize*2;
-              ctx.moveTo(this.tempPosition.x,this.tempPosition.y)
-              ctx.lineTo(pos.x,pos.y)
-              
-            })
-          }
-          
-          this.setCanvasTempPosition(currentPos.x,currentPos.y)
+         
       })
       window.addEventListener('popstate',(e)=>{
-        
         this.canvasContext.putImageData(e.state, 0, 0);
       })
     },
@@ -142,13 +185,14 @@ export default {
     draw(action){
       let canvasContext = this.canvasContext
       canvasContext.beginPath()
+      canvasContext.lineWidth = this.currentSize*2;        
       action(canvasContext)
       canvasContext.stroke()
     },
     resetToolState(){
-      this.isDrawing = false
       this.isSizing = false
       this.tempPosition = null
+      this.isCanvasMouseDown = false
     }, 
     isColorActive(color){
       return this.currentColor && color==this.currentColor.name 
@@ -160,7 +204,7 @@ export default {
     resetCanvas(){
       let canvas = this.canvasContext.canvas
       this.canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-      this.saveCavas()
+      this.saveCanvasToHistory()
     },
     setCanvas(){
       let canvas = this.$refs['sketchpad']
@@ -171,11 +215,17 @@ export default {
       ctx.lineJoin = "round"
       this.canvasContext = ctx
     }, 
-    saveCavas(){
-      let context = this.canvasContext
-      let canvas = context.canvas 
-      let state = context.getImageData(0, 0, canvas.width, canvas.height);
-      window.history.pushState(state, null);
+    setTempCanvas(){ 
+      let ctx = this.canvasContext
+      let canvas = ctx.canvas 
+      let tempCanvas = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      this.tempCanvas = tempCanvas
+    }, 
+    saveCanvasToHistory(){
+      let ctx = this.canvasContext
+      let canvas = ctx.canvas 
+      let tempCanvas = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      window.history.pushState(tempCanvas, null);
     },
     getCanvasMousePosition(clientX,clientY){
       let canvasPosition = this.canvasContext.canvas.getBoundingClientRect()
@@ -223,8 +273,13 @@ export default {
   text-align: center;
   line-height: 60px;
   height: 60px;
-   
-
+  position: fixed;
+  left: 50%; 
+  top: 0;
+  transform: translateX(-50%);
+  width: 100%;
+  background-color: white;
+  transition: all 0.1s ;
   li{
     font-size: 20px; 
     margin:0 40px;
@@ -232,13 +287,13 @@ export default {
     display: inline-block;
     list-style: none;
     &:hover{
-    background-color: #e2e2e2;
+    background-color: #efefef;
      cursor: pointer;
     }
   }
  
 }
- 
+
  
 .toolbar{
   position: fixed;
@@ -298,8 +353,7 @@ export default {
   }
 }
 
-.hideToolBar{
-  display: none;
-}
  
+
+
 </style>
